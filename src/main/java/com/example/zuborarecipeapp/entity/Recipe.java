5fd5f6fc78209041241cd3f3e3398ae86e6ee6fc
sbko787878 <s.b.ko787878@gmail.com>
2026-02-06 16:05:15 +0900

    model実装 テーブル名:zubora_recipes カラム⇒id,name,description,material,image_path

---
+++ src/main/java/com/example/zuborarecipeapp/entity/Recipe.java
@@ -0,0 +1,17 @@
+package com.example.zuborarecipeapp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Recipe {
+	
+	private int id;
+	private String name;
+	private String description;
+	private String material;
+	private String imagePath;
+}

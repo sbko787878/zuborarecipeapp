fa0ac01d011667e9300ad8029bf3ddf8c97d42c8
sbko787878 <s.b.ko787878@gmail.com>
2026-02-06 11:44:13 +0900

    プロジェクト初回コミット

---
+++ src/test/java/com/example/zuborarecipeapp/ZuborarecipeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zuborarecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuborarecipeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c436829b339fd21b5748cb74a81e18b2aabbaaf9
sbko787878 <s.b.ko787878@gmail.com>
2026-02-13 13:59:51 +0900

    no message

--- src/main/java/com/example/zuborarecipeapp/service/RecipeServiceImpl.java
+++ src/main/java/com/example/zuborarecipeapp/service/RecipeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.zuborarecipeapp.service;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -14,20 +15,44 @@ import lombok.RequiredArgsConstructor;
 public class RecipeServiceImpl implements RecipeService {
 
 	private final RecipeMapper recipeMapper;
-	
-	@Override
-	public String breakBySpace(String title) {
-        return title.replace(" ", "<br>");
-    }
 
 	@Override
 	public List<Recipe> getAllRecipes() {
 
-		return recipeMapper.getAllRecipes();
+		List<Recipe> recipeList=recipeMapper.getAllRecipes();
+		// 取得した全データに対して加工を行う
+        for (Recipe recipe : recipeList) {
+            // 1. スペースを<br>に変換して上書き
+//        	breakBySpace(recipe);
+            // 2. iconPath(文字列)をiconList(List)に変換
+//            setupIconList(recipe);
+        }
+		return recipeList;
+		
 	}
 
 	@Override
 	public Recipe findRecipeById(int id) {
-		return recipeMapper.findRecipeById(id);
+		Recipe recipe=recipeMapper.findRecipeById(id);
+		if (recipe != null) {
+			//DB側で改行するよう改善中
+//			breakBySpace(recipe);
+//			setupIconList(recipe);
+		}
+		return recipe;
 	}
+	
+	// 3. 内部処理（Private）
+    // DB側で改行改善中
+    private void breakBySpace(Recipe recipe) {
+        recipe.setName(((recipe.getName()).replace(" ", "<br>")));
+    }
+
+    private void setupIconList(Recipe recipe) {
+    	if (recipe.getIconPath() != null && !recipe.getIconPath().isEmpty()) {
+            recipe.setIconList(Arrays.asList(recipe.getIconPath().split(",")));
+        } else {
+            recipe.setIconList(new java.util.ArrayList<>());
+        }
+    }
 }

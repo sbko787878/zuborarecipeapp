d1752064e872a872e41cff4935acdd24bdd29997
sbko787878 <s.b.ko787878@gmail.com>
2026-02-25 16:14:46 +0900

    ＜feat＞ログアウトボタン及びパスワードハッシュ化実装

--- src/main/java/com/example/zuborarecipeapp/controller/LoginController.java
+++ src/main/java/com/example/zuborarecipeapp/controller/LoginController.java
@@ -1,29 +1,31 @@
 package com.example.zuborarecipeapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.example.zuborarecipeapp.service.AdminUserService;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
-	private final AdminUserService service;
+//	private final AdminUserService service;
 
-    @PostMapping("/login")
-    public String login(
-    		@RequestParam String name,
-    		@RequestParam String pass) {
-    	service.search(name,pass);
-    	if(service.search(name,pass) == null) {
-    		return "login";
-    	}
-        return "loginComp"; // login.html を表示
-    }
-
+//    @PostMapping("/login")
+//    public String login(
+//    		@RequestParam String name,
+//    		@RequestParam String pass,
+//    		HttpSession session) {
+//    	AdminUser adminUser = service.search(name, pass);
+//    	if(adminUser == null) {
+//    		return "redirect:/login?error";
+//    	}
+//    	
+//    	// セッションにログインユーザー情報を保存
+//        // "loginUser" という名前（キー）で保存します
+//        session.setAttribute("adminUser", adminUser);
+//    	
+//        return "redirect:/admin/recipes"; // 直接表示ではなく、メニューへリダイレクトするのが一般的
+//    }
+//
     @GetMapping("/login")
     public String loginComp() {
         return "login"; // loginComp.html を表示

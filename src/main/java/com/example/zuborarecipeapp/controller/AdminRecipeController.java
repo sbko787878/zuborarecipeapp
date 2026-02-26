7a285f0decd847d1ed52513caa7dafc80d9f1546
sbko787878 <s.b.ko787878@gmail.com>
2026-02-26 11:19:01 +0900

    ＜feat＞spring seccurity実装（/admin/*） (予定)中分類のアイコン修正

--- src/main/java/com/example/zuborarecipeapp/controller/AdminRecipeController.java
+++ src/main/java/com/example/zuborarecipeapp/controller/AdminRecipeController.java
@@ -8,11 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.zuborarecipeapp.entity.AdminUser;
 import com.example.zuborarecipeapp.entity.Recipe;
 import com.example.zuborarecipeapp.service.RecipeService;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -30,41 +28,21 @@ public class AdminRecipeController {
 
 	// 新規登録フォーム
 	@GetMapping("/new")
-	public String newForm(Model model,HttpSession session) {
-		// セッションから情報を取り出す
-		AdminUser loginUser = (AdminUser) session.getAttribute("adminUser");
-		if (loginUser == null) {
-			// セッションが空ならログイン画面へ強制送還
-			return "redirect:/login";
-		}
+	public String newForm(Model model) {
 		model.addAttribute("recipe", new Recipe());
 		return "admin/recipeform";
 	}
 
 	// 編集フォーム
 	@GetMapping("/edit/{id}")
-	public String editForm(@PathVariable int id, Model model,HttpSession session) {
-		// セッションから情報を取り出す
-		AdminUser loginUser = (AdminUser) session.getAttribute("adminUser");
-		if (loginUser == null) {
-			// セッションが空ならログイン画面へ強制送還
-			return "redirect:/login";
-		}
-
+	public String editForm(@PathVariable int id, Model model) {
 		model.addAttribute("recipe", recipeService.findRecipeById(id));
 		return "admin/recipeform";
 	}
 
 	// 保存（新規 or 更新）
 	@PostMapping("/save")
-	public String save(@ModelAttribute Recipe recipe,HttpSession session) {
-		// セッションから情報を取り出す
-		AdminUser loginUser = (AdminUser) session.getAttribute("adminUser");
-		if (loginUser == null) {
-			// セッションが空ならログイン画面へ強制送還
-			return "redirect:/login";
-		}
-
+	public String save(@ModelAttribute Recipe recipe) {
 		if (recipe.getId() == null) {
 			recipeService.insertRecipe(recipe); // 新規
 		} else {
@@ -75,14 +53,8 @@ public class AdminRecipeController {
 	}
 
 	// 削除
-	@GetMapping("/delete/{id}")
-	public String delete(@PathVariable int id,HttpSession session) {
-		// セッションから情報を取り出す
-		AdminUser loginUser = (AdminUser) session.getAttribute("adminUser");
-		if (loginUser == null) {
-			// セッションが空ならログイン画面へ強制送還
-			return "redirect:/login";
-		}
+	@PostMapping("/delete/{id}")
+	public String delete(@PathVariable int id) {
 		recipeService.deleteRecipe(id);
 		return "redirect:/admin/recipes";
 	}
